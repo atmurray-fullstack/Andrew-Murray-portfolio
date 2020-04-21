68b795a41efaa5989e87593375412846fcd21f6b
Andrew Murray <atmurray@bellsouth.net>
2020-04-21 14:23:00 -0400

    attaching links to anchors in html/ also adding pictures to links and attaching click events to anchors

---
+++ script.js
@@ -0,0 +1,13 @@
+$("#exchange").on("click",function(event){
+    console.log(event.currentTarget);
+    event.preventDefault();
+    window.open("https://mazdamike.github.io/Cryptocurrency-Exchange-and-News-Tracker/");
+    window.open("https://github.com/mazdamike/Cryptocurrency-Exchange-and-News-Tracker");
+});
+
+$("#weather").on("click",function(event){
+    console.log(event);
+    event.preventDefault();
+    window.open("https://atmurray-fullstack.github.io/Weather-Forecase-Info-app/");
+    window.open("https://github.com/atmurray-fullstack/Weather-Forecase-Info-app");
+})
