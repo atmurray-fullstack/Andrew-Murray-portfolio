eeb971120690aae3f911dab857b543b2fcae40b2
Andrew Murray <atmurray@bellsouth.net>
2020-04-23 11:44:19 -0400

    reduced project divs added edited bootstrap and about me page adding github and linkedIn icons and links

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 $("#exchange").on("click",function(event){
-    console.log(event.currentTarget);
-    event.preventDefault();
-    window.open("https://mazdamike.github.io/Cryptocurrency-Exchange-and-News-Tracker/");
-    window.open("https://github.com/mazdamike/Cryptocurrency-Exchange-and-News-Tracker");
+event.preventDefault();
+window.open("https://mazdamike.github.io/Cryptocurrency-Exchange-and-News-Tracker/");
 });
 
 $("#weather").on("click",function(event){
-    console.log(event);
     event.preventDefault();
-    window.open("https://atmurray-fullstack.github.io/Weather-Forecase-Info-app/");
-    window.open("https://github.com/atmurray-fullstack/Weather-Forecase-Info-app");
+    window.open("https://atmurray-fullstack.github.io/Weather-Forecast-Info-app/");
+   
+})
+
+$("#planner").on("click",function(event){
+    event.preventDefault();
+    window.open("https://atmurray-fullstack.github.io/DayPlanner-ATM/");
+   
 })
